b876d4f9850fb4ad4a13e4fb8508a80de45606af
stemirgaly <113301230+stemirgaly@users.noreply.github.com>
2023-01-14 19:58:04 +0600

    full done

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ from body import gamePanel
 from body import game
 
 
-
-
-
-
-
 if __name__ == '__main__':
     size = 4
     grid = grid.Grid(size)
     panel = gamePanel.GamePanel(grid)
     game2048 = game.Game(grid, panel)
     game2048.start()
+
